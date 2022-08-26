fc207d40dcfeba476c7084e25c207bf33528981f
Mike Engelhardt <mengelhardt@yahoo.com>
2022-08-26 15:08:43 -0400

    Updates

---
+++ precache-manifest.f2beed9a05f5411e9b0f3f2698c763cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94322ecc9f27e25ccdd80570e28446b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b82528ddc664a51eb336",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "c61108a3648297ca3aeb",
+    "url": "./static/js/2.9ef25971.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9ef25971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82528ddc664a51eb336",
+    "url": "./static/js/main.1d26a95a.chunk.js"
+  },
+  {
+    "revision": "c5e5eb901b6872807ec5",
+    "url": "./static/js/runtime-main.2bf3b26f.js"
+  }
+]);
